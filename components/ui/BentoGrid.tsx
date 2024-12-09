f43e821fa243f1dce3be427bc0c9c8cc1295bc35
Rajat-pixel-codes <65059677+Rajat-pixel-codes@users.noreply.github.com>
2024-12-09 14:33:04 +0530

    third commit

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -4,11 +4,13 @@
 import { cn } from '@/lib/utils';
 import { BackgroundGradientAnimation } from './GradientBg';
 import { GlobeDemo } from './GridGlobe';
-import Lottie from 'react-lottie';
 import { useState } from 'react';
 import animationData from '@/data/confetti.json';
 import MagicButton from './MagicButton';
 import { IoCopyOutline } from 'react-icons/io5';
+import dynamic from 'next/dynamic';
+
+const Lottie = dynamic(() => import('react-lottie'), { ssr: false });
 
 export const BentoGrid = ({
   className,
